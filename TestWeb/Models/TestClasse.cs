b2036a9d3b148a8e9ad1c430faa42185a65751d6
tiagopatriciosantos <tiagopatriciosantos@gmail.com>
2015-05-25 13:59:12 +0100

    AddedClasse

---
+++ TestWeb/Models/TestClasse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWeb.Models
+{
+    public class TestClasse
+    {
+        public int MyProperty { get; set; }
+    }
+}
